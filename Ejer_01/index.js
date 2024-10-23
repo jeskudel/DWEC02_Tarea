fa11eff500396910e2e56117de98d0e33750d1ef
Josu <jeskudel@educacion.navarra.es>
2024-10-23 08:31:58 +0200

    initial commit

---
+++ Ejer_01/index.js
@@ -0,0 +1,8 @@
+// debereis utilizar esta variable para generar el código aleatoriamente
+const hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+
+
+
+function cambiarColor(){
+    // Código a ejecutar cuando se pulsa el botón
+}
---
+++ Ejer_02/index.js
@@ -0,0 +1,17 @@
+deportesClub=["snooker", "tenis", "balonmano", "baloncesto", "futbol", "waterpolo", "MTB"]
+deportesNuevos=["minigolf", "dardos", "eskoba","trinkete","beisbol","curling","ajedrez"]
+
+function crearUsuario(){
+    //no borrar estas líneas, hace que aparezcan los botones necesarios para el ejercicio
+    let botonesUsuario = document.getElementById("botonesUsuario")
+    botonesUsuario.classList.remove("oculto")
+
+
+}
+
+function comprobarDeportes(){
+    //no borrar estas líneas, hace que aparezcan los botones necesarios para el ejercicio
+    let botonesArray = document.getElementById("botonesArray")
+    botonesArray.classList.remove("oculto")
+}
+
